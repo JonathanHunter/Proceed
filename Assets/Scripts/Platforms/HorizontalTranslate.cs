17aea27f319bc4c82a4d19cc67a68cd066fde575
Coates, Justin Thomas <jcoates8@gatech.edu>
2015-09-22 16:32:12 -0400

    Added a few simple platforms for testing
    - A stationary platform
    - A large stationary platform
    - Platforms that translate horizontally and vertically
    - Set up a basic scene with some platforms

---
+++ Assets/Scripts/Platforms/HorizontalTranslate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalTranslate : MonoBehaviour {
+    public float speed, maxTranslate;
+
+    float translated;
+
+	// Use this for initialization
+	void Start () {
+        translated = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed, 0f, 0f);
+        translated += Mathf.Abs(speed);
+
+        if (translated >= maxTranslate) {
+            speed *= -1;
+            translated = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms/VerticalTranslate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalTranslate : MonoBehaviour {
+    public float speed, maxTranslate;
+
+    float translated;
+
+    // Use this for initialization
+    void Start () {
+        translated = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0f, speed, 0f);
+        translated += Mathf.Abs(speed);
+
+        if (translated >= maxTranslate) {
+            speed *= -1;
+            translated = 0.0f;
+        }
+    }
+}
