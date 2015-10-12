9653af0ec2481de6e53406b508b9fe166c446103
Coates, Justin Thomas <jcoates8@gatech.edu>
2015-10-12 01:06:40 -0400

    Initial version of icy game feel garden
    Used the Big Platform to make a simple ice level. Balls spawn at one end
    of the platform and roll to the other.
    The platform has very low friction, using a physic material, so Marco
    slides when he runs.

---
+++ Assets/Scripts/EntityBehavior/Enemies/Bowler/BowlerControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BowlerControl : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Transform platform;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (this.transform.position.y <= -20)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(platform.right * speed);
+    }
+}
---
+++ Assets/Scripts/EntityBehavior/Enemies/Bowler/BowlerSpawn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BowlerSpawn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject bowler;
+    [SerializeField]
+    private Transform[] spawnPoints;
+    [SerializeField]
+    private float spawnDelay;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(SpawnBowlers());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    IEnumerator SpawnBowlers()
+    {
+        while (true)
+        {
+            Instantiate(bowler, spawnPoints[Random.Range(0, spawnPoints.Length - 1)].position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
