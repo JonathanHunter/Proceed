06d80996f3e3e55704f60a754c7f4fae5dcd2b24
Spex130 <Gamerguy56@bellsouth.net>
2015-10-13 15:51:24 -0400

    Created a Ragdoll and a method of calling it.
    Will get back to this in a second but the ControllerInputManager is
    throwing like 21 errors and I can't build because of it.

---
+++ Assets/Scripts/Player/ragdollBody.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ragdollBody : MonoBehaviour {
+
+	public float destructionTimer = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(destructionTimer >0){
+			destructionTimer -= Time.DetlatTime;
+		}
+		else{
+			Destroy(this);
+		}
+	}
+}
